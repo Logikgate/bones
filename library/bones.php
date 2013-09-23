8328dd8194deb5865c481b45add10ceb7ae01f99
nfredricks <nfredricks@gmail.com>
2013-09-23 15:46:11 -0400

    Allows custom loops to use bones_page_navi

--- library/bones.php
+++ library/bones.php
@@ -311,19 +311,20 @@ PAGE NAVI
 *********************/
 
 // Numeric Page Navi (built into the theme by default)
-function bones_page_navi() {
+function bones_page_navi( $custom_loop_max = 0 ) {
 	global $wp_query;
 	$bignum = 999999999;
-	if ( $wp_query->max_num_pages <= 1 )
+	$max_num_pages = ($custom_loop_max == 0) ? $wp_query->max_num_pages : $custom_loop_max;
+	if ( $max_num_pages <= 1 )
 	return;
-	
+
 	echo '<nav class="pagination">';
 	
 		echo paginate_links( array(
 			'base' 			=> str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
 			'format' 		=> '',
 			'current' 		=> max( 1, get_query_var('paged') ),
-			'total' 		=> $wp_query->max_num_pages,
+			'total' 		=> $max_num_pages,
 			'prev_text' 	=> '&larr;',
 			'next_text' 	=> '&rarr;',
 			'type'			=> 'list',
